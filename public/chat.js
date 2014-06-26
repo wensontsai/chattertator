c43075475a13f965a04526a176e1205186e5446e
Wenson Tsai <wensontsai@gmail.com>
2014-06-26 01:08:35 -0400

    first committtt

---
+++ public/chat.js
@@ -0,0 +1,46 @@
+window.onload = function() {
+  // when press ENTER key, sends chat message
+  $("#field").keyup(function(e){
+    if(e.keyCode == 13){
+        sendMessage();
+    }
+  });
+
+
+    var messages = [];
+    var socket = io.connect('http://localhost:3700');
+    var field = document.getElementById("field");
+    var sendButton = document.getElementById("send");
+    var content = document.getElementById("content");
+    var name = document.getElementById("name");
+
+    socket.on('message', function (data) {
+        if(data.message) {
+            messages.push(data);
+            var html = '';
+            for(var i=0; i<messages.length; i++) {
+                html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+                html += messages[i].message + '<br />';
+            }
+            content.innerHTML = html;
+            content.scrollTop = content.scrollHeight;
+
+            // for IE7 compatability
+            $("#content").scrollTop($("#content")[0].scrollHeight);
+
+        } else {
+            console.log("There is a problem:", data);
+        }
+    });
+
+    sendButton.onclick = sendMessage = function() {
+        if(name.value == "") {
+            alert("Please type your name!");
+        } else {
+            var text = field.value;
+            socket.emit('send', { message: text, username: name.value });
+            field.value = '';
+        }
+    };
+
+}
